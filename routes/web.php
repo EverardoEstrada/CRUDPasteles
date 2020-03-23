b8aed26ad54f07ce4330efab4b17b15038233c7c
Everardo <verxiel@gmail.com>
2020-03-22 21:49:06 -0600

    CRUD Pasteles lento

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+Route::get('/', 'PastelController@muestra');
+Route::get('/edita/{id}', 'PastelController@editaPastel');
+Route::get('/borra/{id}', 'PastelController@borra');
+
+Route::post('/guardaPastel','PastelController@guardaPastel');
+Route::post('/GuardaEdicion','PastelController@GuardaEdicion');
